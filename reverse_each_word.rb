b2c3adbea7bdd39c486d6720dd5e3dac47240ab5
Cara Morelli <caramorelli7@gmail.com>
2018-09-24 23:32:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(string)
-  rev_arr = []
+  # rev_arr = []
   string = string.split(' ')
   string.collect do |word|
-    rev_arr << word.reverse
+    # rev_arr << word.reverse
+    word.reverse
   end 
-  rev_arr.join(' ')
+  # rev_arr.join(' ')
 end 
